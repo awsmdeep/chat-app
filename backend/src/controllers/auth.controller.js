fd84f7f3404adf95040b814e609c5f98b3792947
deepak <deepakdas538@gmail.com>
2025-02-03 03:44:20 +0530

    added the signup controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,14 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 
 export const signup= async (req,res)=>{
     const {fullName,email,password}=req.body
     try {
+
+        if(!fullName || !email || !password){
+            return res.status(400).json({message:"Enter all the crediatials"})
+        }
         if(password.length<6){
             return res.status(400).json({message:"Password must be at least 6 characters"});
         }
@@ -19,12 +24,20 @@ export const signup= async (req,res)=>{
         })
 
         if(newUser){
-
+            generateToken(newUser._id,res)
+            await newUser.save();
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:fullName,
+                email:email,
+                profilePic:newUser.profilePic
+            })
         }else{
-            return res.status(400).json({message:"Invalid user data"})
+             res.status(400).json({message:"Invalid user data"})
         }
     } catch (error) {
-        
+        console.log("Error in signup controller",error.message);
+        res.status(500).json({message:"Internal Server Error"})
     }
 }
 
